af730ea16b04b2c1e4ba6b8c3776c850c42f7c9e
Danish Ansari <ansarid567@gmail.com>
2021-01-22 18:22:08 +0530

    Written test for number functions

---
+++ extension/src/test/java/org/nvest/extension/NumberKtTest.kt
@@ -0,0 +1,51 @@
+package org.nvest.extension
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class NumberKtTest {
+    @Test
+    fun `Number is rounded properly`() {
+        val input = 123.456
+
+        val result = input.round(2)
+
+        assertThat(result).isEqualTo(123.46)
+    }
+
+    @Test
+    fun `Double in String is a number`() {
+        val input = "123.456"
+
+        val result = input.isNumber()
+
+        assertThat(result).isTrue
+    }
+
+    @Test
+    fun `Integer in String is a number`() {
+        val input = "123"
+
+        val result = input.isNumber()
+
+        assertThat(result).isTrue
+    }
+
+    @Test
+    fun `Text in String is not a number`() {
+        val input = "123Abc"
+
+        val result = input.isNumber()
+
+        assertThat(result).isFalse
+    }
+
+    @Test
+    fun `null is not a number`() {
+        val input = null
+
+        val result = input.isNumber()
+
+        assertThat(result).isFalse
+    }
+}
